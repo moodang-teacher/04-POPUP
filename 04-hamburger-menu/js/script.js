855c2f7aca981ce3daa681eaf283d37229c7e100
moodang <yssmi@naver.com>
2023-12-05 11:45:03 +0900

    공통의 동작을 함수로 정의

--- 04-hamburger-menu/js/script.js
+++ 04-hamburger-menu/js/script.js
@@ -20,24 +20,22 @@ $(function () {
     // $menu가 보여지게 : isActive 조건에 따라서
     if (isActive === false) {
       // $menu를 보이게
-      $menu.animate(
-        {
-          left: 0,
-        },
-        350
-      );
-
+      slideMenu(0);
       isActive = true;
     } else {
       // $menu를 안 보이게
-      $menu.animate(
-        {
-          left: '-100%',
-        },
-        350
-      );
-
+      slideMenu('-100%');
       isActive = false;
     }
   });
+
+  // 공통의 동작을 함수로 정의
+  function slideMenu(pos) {
+    $menu.animate(
+      {
+        left: pos,
+      },
+      350
+    );
+  }
 });
