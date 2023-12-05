1eb9ac3bc1fba60c4bcc176c361ce1ea9facbf25
moodang <yssmi@naver.com>
2023-12-05 13:03:31 +0900

    서브 메뉴 완성

--- 04-hamburger-menu/js/script.js
+++ 04-hamburger-menu/js/script.js
@@ -22,7 +22,7 @@ $(function () {
   // 공통의 동작을 함수로 정의
   // 1. 메뉴의 움직임 (보이거나 숨기거나)
   function slideMenu(pos) {
-    $menu.animate(
+    $menu.stop().animate(
       {
         left: pos,
       },
@@ -33,7 +33,7 @@ $(function () {
   // 2. openMenu : 메뉴를 보이게(메뉴, active부여, dim fadeIn, isActive)
   function openMenu() {
     $btnMenu.addClass('active');
-    $dim.fadeIn();
+    $dim.stop().fadeIn();
     slideMenu(0);
     isActive = true;
   }
@@ -41,8 +41,29 @@ $(function () {
   // 3. closeMenu : 메뉴를 안 보이게(메뉴, active삭제, dim fadeOut, isActive)
   function closeMenu() {
     $btnMenu.removeClass('active');
-    $dim.fadeOut();
+    $dim.stop().fadeOut();
     slideMenu('-100%');
     isActive = false;
+
+    // 서브메뉴 초기화
+    initSubmenu();
+  }
+
+  // 서브메뉴 동작
+  const $menuList = $('.menu > li');
+  const $submenu = $('.submenu');
+
+  $menuList.on('click', function (e) {
+    e.preventDefault();
+    initSubmenu();
+
+    $(this).toggleClass('on');
+    $(this).find($submenu).stop().slideToggle();
+  });
+
+  // 서브메뉴 초기화를 함수로 분리
+  function initSubmenu() {
+    $submenu.stop().slideUp();
+    $menuList.removeClass('on');
   }
 });
